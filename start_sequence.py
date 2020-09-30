ba3a9709c86fca8524a654c3c5e4c59073eca911
oscar boutin <oscar@boutco.com>
2020-09-30 18:44:25 +0200

    ''

---
+++ start_sequence.py
@@ -0,0 +1,15 @@
+
+
+class ProcessNode:
+	def __init__ (self, required_input):
+		self.input_dict = {}
+		self.output_dict = {}
+		self.output_links = []
+		self.required_input = []
+	
+	def run (self):
+		pass
+	
+
+if __name__ == "__main__":
+	print("coucou")
